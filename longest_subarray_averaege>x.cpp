a398d72a470ff7d18a26b49c03eb7fcdda6c0eca
BANGARU SAI JAGADEESH <39492354+pezzonovante7@users.noreply.github.com>
2019-07-15 16:17:55 +0530

    Create longest_subarray_averaege>x.cpp

---
+++ longest_subarray_averaege>x.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+int max_subarray(int arr[],int x,int n) 
+{ 
+    int length=0 ,temp =0 ; 
+    int max=0,sum=0,i; 
+    for(i=0;i<n;i++) 
+    {  
+        sum = sum+arr[i]; 
+        temp++; 
+        if(sum>max && sum/temp>=x && temp>=length) 
+        { 
+            max= sum ; 
+            length =temp; 
+        } 
+        if(sum<0) 
+        { 
+            sum=0; 
+            temp=0; 
+        } 
+          
+    } 
+  
+return length;  
+} 
+int main() { 
+int arr[] = {-2, 1, 6, -3 }; 
+int x = 2; 
+int n = sizeof(arr)/sizeof(int); 
+int length = max_subarray(arr,x,n); 
+printf("%d",length); 
+return 0; 
+} 
